5acc2cac4336cc5363f9cfa74e757d0bd34a63da
zhaoning <1102386745@qq.com>
2021-08-13 16:30:40 +0800

    'goroutine'

--- code/08_并发之goroutine/01_goroutine/goroutine.go
+++ code/08_并发之goroutine/01_goroutine/goroutine.go
@@ -9,6 +9,15 @@ func hello() {
 	fmt.Println("hello go")
 }
 
+
+// go协程的特点
+// 1.有独立的栈空间
+// 2.共享程序堆空间
+// 3.调度由用户控制
+// 4.协程事故轻量级的线程
+
+// MPG
+
 // 在程序启动时，Go程序就会为main()函数创建一个默认的goroutine
 func main() {
 	go hello() // 启动另外一个goroutine去执行hello函数
--- code/08_并发之goroutine/03_GOMAXPROCS/GOMAXPROCS.go
+++ code/08_并发之goroutine/03_GOMAXPROCS/GOMAXPROCS.go
@@ -20,6 +20,8 @@ func b() {
 // Go运行时的调度器使用GOMAXPROCS参数来确定需要使用多少个OS线程来同时执行Go代码。
 // 默认值是机器上的CPU核心数。例如在一个8核心的机器上，调度器会把Go代码同时调度到8个OS线程上（GOMAXPROCS是m:n调度中的n）
 func main() {
+	fmt.Println(runtime.NumCPU()) //CPU数量
+
 	// runtime.GOMAXPROCS(1)
 	runtime.GOMAXPROCS(2)
 	go a()
